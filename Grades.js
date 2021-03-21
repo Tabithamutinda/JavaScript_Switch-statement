2591e8ed198aa7f9ee3c728ae76e128569a2296c
Tabithamutinda <tabbithamutinda@gmail.com>
2021-03-21 15:32:53 +0300

    Js switch

---
+++ Grades.js
@@ -0,0 +1,22 @@
+function getGrade(){
+var marks=84;
+if (marks>=80){
+    console.log("A");
+}
+else if(marks<=79){
+    console.log("B");
+}
+else if(marks<=69){
+    console.log("C");
+}
+else if (marks<=59){
+    console.log("D");
+}
+else if(marks<=49){
+    console.log("E");
+}
+else {
+    console.log(F);
+}
+}
+getGrade()
